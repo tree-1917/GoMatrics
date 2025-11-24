3de39b9169d9c3001a4ea749304f8d20e3c0c3f9
tree-1917 <gamalmo1917@gmail.com>
2025-11-24 13:03:01 +0200

    first commit

---
+++ router/posts.go
@@ -0,0 +1,16 @@
+package router
+
+// == Get Posts
+func getPosts() {}
+
+// == Create Post
+func createPost() {}
+
+// == Get Post
+func getPost() {}
+
+// == Update Post
+func updatePost() {}
+
+// == Delete Post
+func deletePost() {}
